fd6c6c0288fd44f95bc1c097038825c001fdc9c0
Sameer Kanakia <skanakia1@gmail.com>
2018-03-23 08:49:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var recipes = {flour: '2 cups'}
+var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   var newobj = Object.assign({}, recipes)
-  
+  newobj[key] = value
+  return newobj
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+functi
